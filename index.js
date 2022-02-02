478f3009ee06cce4bab3d3c5a73fb8309305bd93
Ayush Dey <ayushdey0970@gmail.com>
2022-02-02 20:57:43 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,56 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        MakeSound(this.innerHTML);
+        animation(this.innerHTML);
+    });
+}
+
+document.addEventListener("keypress", function (button) {
+    MakeSound(button.key);
+    animation(button.key);
+})
+
+
+function MakeSound(variable) {
+    switch (variable) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+function animation(key) {
+    var active = document.querySelector("." + key);
+    active.classList.add("pressed");
+    setTimeout(function () {
+        active.classList.remove("pressed");
+    }, 100);
+}
